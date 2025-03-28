de9e4e67cca1a60872f73f09949bb461ba383b0b
tabsanro <dojan789@sju.ac.kr>
2025-03-28 15:43:25 +0900

    add: torch2trt

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 pip install --no-cache-dir -r requirements.txt
 python modules/SelfPose3d/tools/download_from_drive.py
 
+git clone https://github.com/NVIDIA-AI-IOT/torch2trt.git
+cd torch2trt
+python setup.py install
+
+cd ..
